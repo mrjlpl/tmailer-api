b56f0143e55432ac2998ccedf5bff08fb66b58f1
Jarek Lewandowski <mrjl@softleg.pl>
2022-06-18 15:49:23 +0200

    Initial version of API

---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn as uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if '__main__' == __name__:
+    uvicorn.run(app, host="0.0.0.0", port=8001)
